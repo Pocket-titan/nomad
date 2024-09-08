f24dcbb1c297b928bc0630c9b349aa23d946a57f
Jelmar Gerritsen <jelmargerritsen@gmail.com>
2024-09-08 17:50:01 +0200

    Getting ready for runs

--- trajectory/job.sh
+++ trajectory/job.sh
@@ -1,14 +1,17 @@
 #!/bin/sh
 
-#SBATCH --job-name="job_name"
+#SBATCH --job-name="trajectory"
 #SBATCH --partition=compute
-#SBATCH --time=24:00:00
+#SBATCH --time=00:30:00
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=8
+#SBATCH --cpus-per-task=1
 #SBATCH --mem-per-cpu=1G
-#SBATCH --account=research-<faculty>-<department>
+#SBATCH --account=education-ae-msc-ae
+#SBATCH -o /scratch/jelmargerritse/logs/%j-run.out
 
 export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
+
+ROOT=/scratch/jelmargerritse/nomad/trajectory
 
 module load 2023r1
 module load miniconda3
@@ -17,3 +20,5 @@ module load git
 
 ./scratch/jelmargerritse/conda_init.sh
 conda activate tudat-space
+
+srun python $ROOT/main.py
