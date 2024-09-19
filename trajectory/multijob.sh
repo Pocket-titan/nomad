f00944296ad65dcf9f5da88fb912b6b26d34b63f
Jelmar Gerritsen <jelmargerritsen@gmail.com>
2024-09-19 15:15:17 +0200

    New

--- trajectory/multijob.sh
+++ trajectory/multijob.sh
@@ -31,8 +31,7 @@ perform_run() {
 presets=("neptune_1dsm_0" "neptune_1dsm_1" "neptune_1dsm_2" "neptune_1dsm_3" "neptune_1dsm_4")
 
 for preset in ${presets[@]}; do
-  cmd="python $dir/wishlist.py --preset $preset --folder $preset -o && python $dir/main.py --folder $preset"
-  srun --overlap $cmd &
+  srun --overlap $dir/run.sh $preset &
 done
 
 wait
--- trajectory/run.sh
+++ trajectory/run.sh
@@ -5,4 +5,4 @@ preset=$1
 name=${2:-$preset}
 
 $dir/clean_runs.sh $name
-python wishlist.py --preset $preset -o --folder $name && python main.py --folder $name
+python $dir/wishlist.py --preset $preset -o --folder $name && python $dir/main.py --folder $name
